8fdf97a75fa55cfd70ebe06c857a4a594d85b9d5
Jeferson Macedo <jhef.salles@gmail.com>
2023-12-09 01:39:59 -0300

    inicio da tela de criação de novo usuário

--- FRONTEND/FrontEnd-CafeShop/src/appRoutes.tsx
+++ FRONTEND/FrontEnd-CafeShop/src/appRoutes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import LoginPage from "./pages/login";
+import CreateUser from "./pages/createUser";
 
 const AppRoutes = () => {
     return (
@@ -8,6 +9,7 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/novo-usuario" element={<CreateUser />} />
             </Routes>
         </Router>
     )
---
+++ FRONTEND/FrontEnd-CafeShop/src/pages/createUser.tsx
@@ -0,0 +1,74 @@
+import clsx from "clsx"
+
+const createUser = () => {
+    return (
+        <div className="bg-gray-50 flex justify-center items-center py-5">
+            <main className="w-full max-w-[600px]">
+                <form
+                    action=""
+                    className="flex flex-col gap-4"
+                >
+                    <div className="flex flex-col gap-2">
+                        <label
+                            className="font-sans font-semibold text-base text-gray-800"
+                            htmlFor="email"
+                        >
+                            E-mail
+                        </label>
+                        <input
+                            className={clsx('px-4 py-3 bg-white text-sm text-gray-800 leading-5 border border-gray-200 rounded placeholder:text-gray-200 outline-none focus:border-orange-500', {
+                                // 'border-red': errors.email,
+                                // 'focus:border-red': errors.email
+                            })}
+                            type="email"
+                            id="email"
+                            placeholder="Digite seu e-mail"
+                        // {...register('email')}
+                        />
+                        {/* {errors.email && (<span className="text-red text-sm">{errors.email?.message}</span>)} */}
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <label
+                            className="font-sans font-semibold text-base text-gray-800"
+                            htmlFor="password"
+                        >
+                            Senha
+                        </label>
+                        <input
+                            className={clsx('px-4 py-3 bg-white text-sm text-gray-800 leading-5 border border-gray-200 rounded placeholder:text-gray-200 outline-none focus:border-orange-500')}
+                            type="password"
+                            id="password"
+                            placeholder="Insira uma senha"
+                        />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <label
+                            className="font-sans font-semibold text-base text-gray-800"
+                            htmlFor="confirmPassword"
+                        >
+                            Confirme sua senha
+                        </label>
+                        <input
+                            className={clsx('px-4 py-3 bg-white text-sm text-gray-800 leading-5 border border-gray-200 rounded placeholder:text-gray-200 outline-none focus:border-orange-500')}
+                            type="password"
+                            id="confirmPassword"
+                            placeholder="Digite novamente a senha"
+                        />
+
+                        <footer className="flex flex-col gap-8 mt-5">
+                            <button
+                                className="bg-orange-500 text-white font-bold py-4 rounded outline-none hover:bg-orange-400 hover:ring-1 hover:ring-orange-500 focus:ring-2 focus:ring-orange-400"
+                            >
+                                Cadastrar
+                            </button>
+                        </footer>
+                    </div>
+                </form>
+            </main>
+        </div>
+    )
+}
+
+export default createUser
